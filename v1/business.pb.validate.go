24c1ee470bb3efba8157a27311ee9582566e9348
surendra kandel <surenl@surendras-MacBook-Pro-2.local>
2022-03-27 18:41:18 -0700

    regenerated

--- v1/business.pb.validate.go
+++ v1/business.pb.validate.go
@@ -290,6 +290,35 @@ func (m *BusinessData) validate(all bool) error {
 	}
 
 	if all {
+		switch v := interface{}(m.GetAdminEmail()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, BusinessDataValidationError{
+					field:  "AdminEmail",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, BusinessDataValidationError{
+					field:  "AdminEmail",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetAdminEmail()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return BusinessDataValidationError{
+				field:  "AdminEmail",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if all {
 		switch v := interface{}(m.GetCreatedOn()).(type) {
 		case interface{ ValidateAll() error }:
 			if err := v.ValidateAll(); err != nil {
