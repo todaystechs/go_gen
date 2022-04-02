2829cb527917bba93632e5aa4684922aa4903d45
surendra kandel <surenl@surendras-MacBook-Pro-2.local>
2022-04-02 10:46:13 -0700

    regenerated

--- v1/user.pb.validate.go
+++ v1/user.pb.validate.go
@@ -334,49 +334,32 @@ func (m *DynamoUserEntity) validate(all bool) error {
 
 	// no validation rules for UserSk
 
-	{
-		sorted_keys := make([]string, len(m.GetUserEntity()))
-		i := 0
-		for key := range m.GetUserEntity() {
-			sorted_keys[i] = key
-			i++
-		}
-		sort.Slice(sorted_keys, func(i, j int) bool { return sorted_keys[i] < sorted_keys[j] })
-		for _, key := range sorted_keys {
-			val := m.GetUserEntity()[key]
-			_ = val
-
-			// no validation rules for UserEntity[key]
-
-			if all {
-				switch v := interface{}(val).(type) {
-				case interface{ ValidateAll() error }:
-					if err := v.ValidateAll(); err != nil {
-						errors = append(errors, DynamoUserEntityValidationError{
-							field:  fmt.Sprintf("UserEntity[%v]", key),
-							reason: "embedded message failed validation",
-							cause:  err,
-						})
-					}
-				case interface{ Validate() error }:
-					if err := v.Validate(); err != nil {
-						errors = append(errors, DynamoUserEntityValidationError{
-							field:  fmt.Sprintf("UserEntity[%v]", key),
-							reason: "embedded message failed validation",
-							cause:  err,
-						})
-					}
-				}
-			} else if v, ok := interface{}(val).(interface{ Validate() error }); ok {
-				if err := v.Validate(); err != nil {
-					return DynamoUserEntityValidationError{
-						field:  fmt.Sprintf("UserEntity[%v]", key),
-						reason: "embedded message failed validation",
-						cause:  err,
-					}
-				}
+	if all {
+		switch v := interface{}(m.GetUserEntity()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, DynamoUserEntityValidationError{
+					field:  "UserEntity",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
 			}
-
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, DynamoUserEntityValidationError{
+					field:  "UserEntity",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetUserEntity()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return DynamoUserEntityValidationError{
+				field:  "UserEntity",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
 		}
 	}
 
