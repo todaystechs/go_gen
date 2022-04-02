8b88d159a6007fe8fee1f0d9589bebc4db04b693
surendra kandel <surenl@surendras-MacBook-Pro-2.local>
2022-04-02 10:21:31 -0700

    regenerated

--- v1/user.pb.validate.go
+++ v1/user.pb.validate.go
@@ -303,3 +303,152 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = MeDataValidationError{}
+
+// Validate checks the field values on DynamoUserEntity with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// first error encountered is returned, or nil if there are no violations.
+func (m *DynamoUserEntity) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on DynamoUserEntity with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// DynamoUserEntityMultiError, or nil if none found.
+func (m *DynamoUserEntity) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *DynamoUserEntity) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Pk
+
+	// no validation rules for Sk
+
+	{
+		sorted_keys := make([]string, len(m.GetUserEntity()))
+		i := 0
+		for key := range m.GetUserEntity() {
+			sorted_keys[i] = key
+			i++
+		}
+		sort.Slice(sorted_keys, func(i, j int) bool { return sorted_keys[i] < sorted_keys[j] })
+		for _, key := range sorted_keys {
+			val := m.GetUserEntity()[key]
+			_ = val
+
+			// no validation rules for UserEntity[key]
+
+			if all {
+				switch v := interface{}(val).(type) {
+				case interface{ ValidateAll() error }:
+					if err := v.ValidateAll(); err != nil {
+						errors = append(errors, DynamoUserEntityValidationError{
+							field:  fmt.Sprintf("UserEntity[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				case interface{ Validate() error }:
+					if err := v.Validate(); err != nil {
+						errors = append(errors, DynamoUserEntityValidationError{
+							field:  fmt.Sprintf("UserEntity[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				}
+			} else if v, ok := interface{}(val).(interface{ Validate() error }); ok {
+				if err := v.Validate(); err != nil {
+					return DynamoUserEntityValidationError{
+						field:  fmt.Sprintf("UserEntity[%v]", key),
+						reason: "embedded message failed validation",
+						cause:  err,
+					}
+				}
+			}
+
+		}
+	}
+
+	if len(errors) > 0 {
+		return DynamoUserEntityMultiError(errors)
+	}
+	return nil
+}
+
+// DynamoUserEntityMultiError is an error wrapping multiple validation errors
+// returned by DynamoUserEntity.ValidateAll() if the designated constraints
+// aren't met.
+type DynamoUserEntityMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m DynamoUserEntityMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m DynamoUserEntityMultiError) AllErrors() []error { return m }
+
+// DynamoUserEntityValidationError is the validation error returned by
+// DynamoUserEntity.Validate if the designated constraints aren't met.
+type DynamoUserEntityValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e DynamoUserEntityValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e DynamoUserEntityValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e DynamoUserEntityValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e DynamoUserEntityValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e DynamoUserEntityValidationError) ErrorName() string { return "DynamoUserEntityValidationError" }
+
+// Error satisfies the builtin error interface
+func (e DynamoUserEntityValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sDynamoUserEntity.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = DynamoUserEntityValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = DynamoUserEntityValidationError{}
