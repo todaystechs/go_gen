33d41362a7a52c8c2516d2274bbe364092e87d5d
surendra kandel <surenl@surendras-MacBook.local>
2022-05-17 12:09:41 -0700

    udpates

--- v1/quote.pb.validate.go
+++ v1/quote.pb.validate.go
@@ -198,38 +198,50 @@ func (m *Quote) validate(all bool) error {
 
 	}
 
-	for idx, item := range m.GetVendorBids() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, QuoteValidationError{
-						field:  fmt.Sprintf("VendorBids[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, QuoteValidationError{
-						field:  fmt.Sprintf("VendorBids[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return QuoteValidationError{
-					field:  fmt.Sprintf("VendorBids[%v]", idx),
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
+	{
+		sorted_keys := make([]string, len(m.GetVendorBids()))
+		i := 0
+		for key := range m.GetVendorBids() {
+			sorted_keys[i] = key
+			i++
 		}
+		sort.Slice(sorted_keys, func(i, j int) bool { return sorted_keys[i] < sorted_keys[j] })
+		for _, key := range sorted_keys {
+			val := m.GetVendorBids()[key]
+			_ = val
 
+			// no validation rules for VendorBids[key]
+
+			if all {
+				switch v := interface{}(val).(type) {
+				case interface{ ValidateAll() error }:
+					if err := v.ValidateAll(); err != nil {
+						errors = append(errors, QuoteValidationError{
+							field:  fmt.Sprintf("VendorBids[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				case interface{ Validate() error }:
+					if err := v.Validate(); err != nil {
+						errors = append(errors, QuoteValidationError{
+							field:  fmt.Sprintf("VendorBids[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				}
+			} else if v, ok := interface{}(val).(interface{ Validate() error }); ok {
+				if err := v.Validate(); err != nil {
+					return QuoteValidationError{
+						field:  fmt.Sprintf("VendorBids[%v]", key),
+						reason: "embedded message failed validation",
+						cause:  err,
+					}
+				}
+			}
+
+		}
 	}
 
 	if len(errors) > 0 {
@@ -308,107 +320,6 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = QuoteValidationError{}
-
-// Validate checks the field values on VendorBids with the rules defined in the
-// proto definition for this message. If any rules are violated, the first
-// error encountered is returned, or nil if there are no violations.
-func (m *VendorBids) Validate() error {
-	return m.validate(false)
-}
-
-// ValidateAll checks the field values on VendorBids with the rules defined in
-// the proto definition for this message. If any rules are violated, the
-// result is a list of violation errors wrapped in VendorBidsMultiError, or
-// nil if none found.
-func (m *VendorBids) ValidateAll() error {
-	return m.validate(true)
-}
-
-func (m *VendorBids) validate(all bool) error {
-	if m == nil {
-		return nil
-	}
-
-	var errors []error
-
-	// no validation rules for VendorBids
-
-	if len(errors) > 0 {
-		return VendorBidsMultiError(errors)
-	}
-
-	return nil
-}
-
-// VendorBidsMultiError is an error wrapping multiple validation errors
-// returned by VendorBids.ValidateAll() if the designated constraints aren't met.
-type VendorBidsMultiError []error
-
-// Error returns a concatenation of all the error messages it wraps.
-func (m VendorBidsMultiError) Error() string {
-	var msgs []string
-	for _, err := range m {
-		msgs = append(msgs, err.Error())
-	}
-	return strings.Join(msgs, "; ")
-}
-
-// AllErrors returns a list of validation violation errors.
-func (m VendorBidsMultiError) AllErrors() []error { return m }
-
-// VendorBidsValidationError is the validation error returned by
-// VendorBids.Validate if the designated constraints aren't met.
-type VendorBidsValidationError struct {
-	field  string
-	reason string
-	cause  error
-	key    bool
-}
-
-// Field function returns field value.
-func (e VendorBidsValidationError) Field() string { return e.field }
-
-// Reason function returns reason value.
-func (e VendorBidsValidationError) Reason() string { return e.reason }
-
-// Cause function returns cause value.
-func (e VendorBidsValidationError) Cause() error { return e.cause }
-
-// Key function returns key value.
-func (e VendorBidsValidationError) Key() bool { return e.key }
-
-// ErrorName returns error name.
-func (e VendorBidsValidationError) ErrorName() string { return "VendorBidsValidationError" }
-
-// Error satisfies the builtin error interface
-func (e VendorBidsValidationError) Error() string {
-	cause := ""
-	if e.cause != nil {
-		cause = fmt.Sprintf(" | caused by: %v", e.cause)
-	}
-
-	key := ""
-	if e.key {
-		key = "key for "
-	}
-
-	return fmt.Sprintf(
-		"invalid %sVendorBids.%s: %s%s",
-		key,
-		e.field,
-		e.reason,
-		cause)
-}
-
-var _ error = VendorBidsValidationError{}
-
-var _ interface {
-	Field() string
-	Reason() string
-	Key() bool
-	Cause() error
-	ErrorName() string
-} = VendorBidsValidationError{}
 
 // Validate checks the field values on Quotes with the rules defined in the
 // proto definition for this message. If any rules are violated, the first
