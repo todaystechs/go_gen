1b2c247634e489260a6f6d8b38a5d1d66be08dab
surendra kandel <surenl@surendras-MacBook.local>
2022-07-02 16:58:12 -0700

    regenerated

--- v1/business.pb.validate.go
+++ v1/business.pb.validate.go
@@ -119,6 +119,37 @@ func (m *Business) validate(all bool) error {
 
 	// no validation rules for Pk
 
+	if all {
+		switch v := interface{}(m.GetAddress()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "Address",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "Address",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetAddress()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return BusinessValidationError{
+				field:  "Address",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	// no validation rules for AddressUpdated
+
 	if len(errors) > 0 {
 		return BusinessMultiError(errors)
 	}
