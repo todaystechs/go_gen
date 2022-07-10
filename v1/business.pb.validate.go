70b17ad92390a02dc735574f1a5aa59dc6d98479
surendra kandel <surenl@surendras-MacBook.local>
2022-07-10 12:37:09 -0700

    regenerated

--- v1/business.pb.validate.go
+++ v1/business.pb.validate.go
@@ -150,6 +150,35 @@ func (m *Business) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetPickupAddress()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "PickupAddress",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "PickupAddress",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetPickupAddress()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return BusinessValidationError{
+				field:  "PickupAddress",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return BusinessMultiError(errors)
 	}
