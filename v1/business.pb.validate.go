ea4df9e08ff80a7bf48885dd0d2627ba3df2fa97
surendra kandel <surenl@surendras-MacBook.local>
2022-07-19 20:13:41 -0700

    regenerated

--- v1/business.pb.validate.go
+++ v1/business.pb.validate.go
@@ -90,35 +90,6 @@ func (m *Business) validate(all bool) error {
 	// no validation rules for NeedsAddressUpdate
 
 	if all {
-		switch v := interface{}(m.GetLocation()).(type) {
-		case interface{ ValidateAll() error }:
-			if err := v.ValidateAll(); err != nil {
-				errors = append(errors, BusinessValidationError{
-					field:  "Location",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		case interface{ Validate() error }:
-			if err := v.Validate(); err != nil {
-				errors = append(errors, BusinessValidationError{
-					field:  "Location",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		}
-	} else if v, ok := interface{}(m.GetLocation()).(interface{ Validate() error }); ok {
-		if err := v.Validate(); err != nil {
-			return BusinessValidationError{
-				field:  "Location",
-				reason: "embedded message failed validation",
-				cause:  err,
-			}
-		}
-	}
-
-	if all {
 		switch v := interface{}(m.GetDefaultPickupAddress()).(type) {
 		case interface{ ValidateAll() error }:
 			if err := v.ValidateAll(); err != nil {
